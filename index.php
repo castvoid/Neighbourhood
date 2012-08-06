a545edf1166ed2128e7d42d4491451bb9a7eaa6c
Harry Jones <i.am@harryj.co.uk>
2012-08-06 16:34:03 +0100

    Updated Index

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html>
+<head>
+<title>Neighbourhood</title>
+<style>
+*{
+	margin: 0;
+	padding: 0;
+	
+}
+body{
+	font-family: "Helvetica Neue", Helvetica, sans-serif;
+	background: url(images/index_bg.jpg) no-repeat center center fixed; 
+	-webkit-background-size: cover;
+	-moz-background-size: cover;
+	-o-background-size: cover;
+	background-size: cover;
+	list-style: none;
+	color: #fff;
+	-webkit-font-smoothing:antialiased;
+	overflow: hidden;
+}
+h1{
+	font-size: 70px;
+	position: absolute;
+	top:00px;
+	left:10px;
+}
+</style>
+</head>
+<body>
+<h1>Neighbourhood</h1>
+<table>
+<tr><td>A</td><td>B</td><td>C</td><td>D</td></tr>
+</table>
+</body>
+</html>
