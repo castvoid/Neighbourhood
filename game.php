bc6082895c9265fd1c26e9078d4d0e1a1025202f
Harry Jones <i.am@harryj.co.uk>
2012-08-07 09:29:23 +0100

    Revert "Added Build Toggle"
    This reverts commit e0dcb57b5c098602726468c11064430204ed5082.

--- game.php
+++ game.php
@@ -8,6 +8,7 @@
 <script src="//maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="game.js"></script>
+<<<<<<< HEAD
        <script language="javascript"> 
 function buildtoggle() {
 	var ele = document.getElementById("buildtoggle");
@@ -22,6 +23,8 @@ function buildtoggle() {
 	}
 } 
 </script>
+=======
+>>>>>>> Revert "Added Build Toggle"
 </head>
 <body onload="initialize();run()">
 <div id="map_canvas"></div> <!-- The Google Map -->
@@ -29,15 +32,10 @@ function buildtoggle() {
 	<div id="bottom_menu">
 		<a class="button" href="#">Global Command A</a>
 		<a class="button" href="#">Review</a>
-		<a class="button" id="displayText" href="javascript:buildtoggle();">Build</a>
+		<a class="button" href="#">Build</a>
 	</div>
 	<div id="sidebar">
 		<div class="stats" id="stats"><h1>No Selection</h1></div>
-        <div class="buildmenu" id="buildmenu">
-        
- 		<div id="buildtoggle" style="display: none"><h1>Build</h1><br />Hello</div>	
-        
-        </div>
 	</div>
 	<div id="popUp">
 		
