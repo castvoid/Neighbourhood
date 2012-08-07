4555bb8da72926db2326b18868ac930002842155
Chris Chapman <chris@hackit.me.uk>
2012-08-07 12:19:49 +0100

    Trying again to fix the problem with auth.

--- backend/auth.php
+++ backend/auth.php
@@ -1,14 +1,14 @@
 <?php
 	session_start();
-	if (!$_POST['username']) {
-		header('Location: ../signin.php?err=1');
-		die();
-	}
 	require_once('inc/db.inc');
 	require_once('inc/hashing.inc');
 	$response = mysql_query("SELECT * FROM users WHERE uname='" . $_POST['username'] . "'");
 	$user = mysql_fetch_array($response);
-
+	
+	if (mysql_num_rows($response) < 1) {
+		header('Location: ../signin.php?err=1');
+	}
+	
 	if (hashPassword($_POST['password']) == $user['upass']) {
 		$_SESSION['nh_uid'] = $user['UID'];
 		header('Location: ../game.php');
