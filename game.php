e0dcb57b5c098602726468c11064430204ed5082
luntdotp <peter@snapki.net>
2012-08-07 09:24:48 +0100

    Added Build Toggle

--- game.php
+++ game.php
@@ -8,6 +8,20 @@
 <script src="//maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 <script src="game.js"></script>
+       <script language="javascript"> 
+function buildtoggle() {
+	var ele = document.getElementById("buildtoggle");
+	var text = document.getElementById("displayText");
+	if(ele.style.display == "block") {
+    		ele.style.display = "none";
+		text.innerHTML = "show";
+  	}
+	else {
+		ele.style.display = "block";
+		text.innerHTML = "hide";
+	}
+} 
+</script>
 </head>
 <body onload="initialize();run()">
 <div id="map_canvas"></div> <!-- The Google Map -->
@@ -15,10 +29,15 @@
 	<div id="bottom_menu">
 		<a class="button" href="#">Global Command A</a>
 		<a class="button" href="#">Review</a>
-		<a class="button" href="#">Build</a>
+		<a class="button" id="displayText" href="javascript:buildtoggle();">Build</a>
 	</div>
 	<div id="sidebar">
 		<div class="stats" id="stats"><h1>No Selection</h1></div>
+        <div class="buildmenu" id="buildmenu">
+        
+ 		<div id="buildtoggle" style="display: none"><h1>Build</h1><br />Hello</div>	
+        
+        </div>
 	</div>
 	<div id="popUp">
 		
