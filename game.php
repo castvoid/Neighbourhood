35d10779653a06d67dcc01476547c0a4aede5de6
Harry Jones <i.am@harryj.co.uk>
2012-08-07 13:45:03 +0100

    Updated modal

--- game.php
+++ game.php
@@ -16,7 +16,6 @@
 <title>Neighbourhood</title>
 <link type="text/css" href="bootstrap.min.css" rel="stylesheet"/>
 <link type="text/css" href="game.css" rel="stylesheet"/>
-
 <link type="text/css" href="font.css" rel="stylesheet"/>
 <script src="//maps.googleapis.com/maps/api/js?sensor=false"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
@@ -24,30 +23,39 @@
 <script src="game.js"></script>
 </head>
 <body onload="initialize();run()">
-<div id="map_canvas"></div> <!-- The Google Map -->
-<div class="ui"> <!-- All the UI elements -->
+<div id="map_canvas">
+</div>
+<!-- The Google Map -->
+<div class="ui">
+	<!-- All the UI elements -->
 	<div id="bottom_menu">
 		<a class="button" href="#">Profile</a>
 		<a class="button" href="/backend/signout.php">Log Out</a>
-	
 	</div>
 	<div id="sidebar">
 		<div class="profile">
 			<ul>
-			<li><i class="icon-user icon-large"></i><span id="playername">Player</span>
-                <li><i class="icon-money icon-large"></i> &pound;<span id="currentmoney">0</span>
-            </ul>
+				<li><i class="icon-user icon-large"></i><span id="playername">Player</span>
+				<li><i class="icon-money icon-large"></i> &pound;<span id="currentmoney">0</span>
+			</ul>
 		</div>
 		<div class="stats" id="stats">
 			<h1>No Selection</h1>
 		</div>
 	</div>
-	<div id="buildBox" onClick="buildBox(0);"></div>
-	<div id="buildMenu">
-    <h1>Build / Destroy</h1><br />
-    This is some text.
-    <div id="buildOptions"></div>
-    </div>
+	<div class="modal hide" id="modal">
+		<div class="modal-header">
+			<h3>City</h3>
+		</div>
+		<div class="modal-body">
+			<p>
+				One fine body…
+			</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#" class="btn" data-dismiss="modal">Close</a>
+		</div>
+	</div>
 </div>
 </body>
 </html>
