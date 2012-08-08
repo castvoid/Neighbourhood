84f9e6360971d566d617f9463380a9bd83a248ec
Chris Chapman <chris@hackit.me.uk>
2012-08-08 11:29:52 +0100

    Returning regions captured as JSON.

--- backend/regions_captured.php
+++ backend/regions_captured.php
@@ -1,18 +1,18 @@
 <?php
 	require_once('inc/db.inc');
-	if ($_POST['uid'] == "CURRENT") {
+	if ($_REQUEST['uid'] == "CURRENT") {
 		session_start();
 		$uid = $_SESSION['nh_uid'];
 	} else {
-		$uid = $_POST['uid'];
+		$uid = $_REQUEST['uid'];
 	}
-	$regionlist = '';
 	for ($i=0;$i<11;$i++) {
+		echo $i;
 		$region = region_details($uid, $i);
 		if ($region['conquerstatus'] == 1) {
 			$regionlist .= $region['RID'] . ',';
 		}
 	}
 	$regionlist = substr($regionlist, 0, -1);
-	echo $regionlist;
+	echo '[' . $regionlist . ']';
 ?>
