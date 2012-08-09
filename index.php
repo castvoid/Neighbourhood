6cd41d80ad34449b964d44aa429c1ea81411c301
Hal Batty <philipabatty@talktalk.net>
2012-08-09 11:38:48 +0100

    pulse!!

--- index.php
+++ index.php
@@ -5,125 +5,115 @@
 <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
 <meta charset="utf-8">
 <style type="text/css">
-*{
-	margin: 0;
-	padding: 0;
-	
-}
-body{
-	font-family: "Helvetica Neue", Helvetica, sans-serif;
-	background: url(images/index_bg.jpg) no-repeat center center fixed; 
-	-webkit-background-size: cover;
-	-moz-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-	list-style: none;
-	color: #fff;
-	-webkit-font-smoothing:antialiased;
-	overflow: hidden;
-}
-h1{
-	font-size: 70px;
-	position: absolute;
-	top:50%;
-	left:50%;
-	width:500px;
-	margin-top: -300px;
-	margin-left: -250px;
-	-moz-text-shadow: 0px 5px 3px #888;
-		-webkit-text-shadow: 0px 5px 3px #888;
-			text-shadow: 0px 5px 3px #888;
-}
-table#regions{
-	position: absolute;
-	top:50%;
-	left:50%;
-	width:600px;
-	height:400px;
-	margin-left: -300px;
-	margin-top:-200px;
-	font-weight: bold;
-	overflow: hidden;
-	-webkit-user-select: none;
-	-moz-user-select: none;
-	user-select:none;
 
+
+* {
+    margin: 0;
+    padding: 0;
+}
+body {
+    font-family: "Helvetica Neue", Helvetica, sans-serif;
+    background: url(images/index_bg.jpg) no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+    list-style: none;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+    overflow: hidden;
+}
+@-moz-keyframes pulsate { 
+    0% { background: rgba(0,255,0,0.4) !important }
+    50% { background: rgba(0,100,0,0.4) !important }
+    100% { background: rgba(0,255,0,0.4) !important }
+}
+h1 {
+    font-size: 70px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 500px;
+    margin-top: -300px;
+    margin-left: -250px;
+    -moz-text-shadow: 0px 5px 3px #888;
+    -webkit-text-shadow: 0px 5px 3px #888;
+    text-shadow: 0px 5px 3px #888;
+}
+table#regions {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 600px;
+    height: 400px;
+    margin-left: -300px;
+    margin-top: -200px;
+    font-weight: bold;
+    overflow: hidden;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+}
+table#regions  td:not(.nosave) {
+    width: 150px;
+    height: 100px;
+    text-align: center;
+    border-bottom: 1px solid rgba(51,51,51,.5);
+    border-right: 1px solid rgba(51,51,51,.5);
+    background: rgba(0,0,0,.1);
+    padding: 10px;
+    box-sizing: border-box;
+    -moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    -webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+}
+table#regions tr:last-child td { border-bottom: none }
+table#regions tr td:last-child { border-right: none }
+td {
+    -webkit-transition: background 0.2s;
+    -moz-transition: background .2s;
+    transition: background .2s;
+}
+footer {
+    width: 500px;
+    position: absolute;
+    bottom: 2px;
+    left: 50%;
+    margin-left: -250px;
+    text-align: center;
+    font-weight: bold;
+    font-size: 13px;
+    -moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    -webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+}
+a { color: #fff }
+    .continue {
+        cursor: default;
+        opacity: 0.5;
+    }
+.continue:not(.nosave) {
+    cursor:  pointer;
+    -moz-animation: pulsate 2s infinite !important;
+    animation: pulsate 2s infinite !important;
+}
+input {
+    padding: 5px;
+    border: none;
+    background: rgba(0,0,0,.1);
+    font-size: 16px;
+    color: #fff;
+    font-weight: bold;
+    font-family: "Helvetica Neue", Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    -webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
+    margin-top: 3px;
+    text-align: center;
 }
 
-table#regions  td{
-	width:150px;
-	height:100px;
-	text-align: center;
-	border-bottom: 1px solid rgba(51,51,51,.5);
-	border-right: 1px solid rgba(51,51,51,.5);
-	background: rgba(0,0,0,.1);
-	padding:10px;
-	box-sizing: border-box;
-	-moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-		-webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-			text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-				
-}
-table#regions td:not(#help):not(.nosave){
-	cursor:pointer !important;
-}
-table#regions td:hover:not(#help):not(.nosave){
-	background: rgba(29, 195, 223, 0.1);
 
-	
-}
-table#regions tr:last-child td{
-	border-bottom:none;
-}
-table#regions tr td:last-child{
-	border-right: none;
-}
-td{
-	-webkit-transition: background 0.2s;
-	-moz-transition:background .2s;
-	transition:background .2s;
-}
-footer{
-	width:500px;
-	position: absolute;
-	bottom:2px;
-	left:50%;
-	margin-left: -250px;
-	text-align: center;
-	font-weight: bold;
-	font-size: 13px;
-	-moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-		-webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-			text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-}
-a {
-	color: #fff;
-}
-.continue.nosave{
-	cursor: default;
-	opacity: 0.5;
-}
-.continue:not(.nosave){
-	background:rgba(0,255,0,0.2) !important;
-}
-.continue:hover:not(.nosave){
-	background: rgba(0,200,0,.2) !important;
-}
-input{
-	padding:5px;
-	border: none;
-	background: rgba(0,0,0,.1);
-	font-size: 16px;
-	color: #fff;
-	font-weight: bold;
-	font-family: "Helvetica Neue", Helvetica, sans-serif;
-		-webkit-font-smoothing:antialiased;
-			-moz-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-		-webkit-text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-			text-shadow: 0px -1px 0px rgba(0,0,0,0.25);
-			margin-top: 3px;
-			text-align: center;
-}
 </style>
 </head>
 <body>
